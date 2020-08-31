b134682b43f1042f28faad2a3f5c238a244429f8
Allison Castaneda <allraec@yahoo.com>
2020-08-30 19:26:53 -0500

    calling data success

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 import './App.css';
 
 const App = () => {
@@ -9,9 +11,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  let [characters, setCharacters] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        setCharacters(res.data.results)
+      })
+      .catch(error => {
+        console.log(error, "error")
+      })
+
+  }, [characters]);
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.map((characters, id) => (
+        <Character key={id} character={characters} />
+      ))}
     </div>
   );
 }
